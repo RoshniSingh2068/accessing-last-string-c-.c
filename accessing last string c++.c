06549fad6371a4170642783c9b15629aaf8970e2
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 10:59:52 +0530

    Create accessing last string c++.c

---
+++ accessing last string c++.c
@@ -0,0 +1,13 @@
+
+// accessing the last character of the string , it is bit different
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string mystring = "Hello";
+    cout<< mystring [mystring.length()-1];
+    return 0;
+}
